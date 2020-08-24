65566192d44beed39e2f731f77f05efb723a8726
Shawn Ritch <sr@webdeveloprz.com>
2020-08-24 12:30:04 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require('inc/init.php');
 if ($page){
   $template_name = preg_replace('[^a-zA-Z0-9-_]', '', $page['template']);
   if (empty($template_name))
-      $template_name = 'default';
+    $template_name = 'default';
     $template_file = 'inc/templates/'.$template_name.'.php';
   if (file_exists($template_file)){
     require($template_file);
