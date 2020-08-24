9b5dece7aea39577e84a2acd84bd219669ee8cff
Shawn Ritch <sr@webdeveloprz.com>
2020-08-24 12:26:26 -0700

    Delete wdrzcms.sql

--- cms/wdrzcms.sql
+++
@@ -1,7 +0,0 @@
-In the root/page/images folder you can find the screenshot of the database I created so you can build it as I did...
-Then copy and paste the SQL code below to create the home page
-
-INSERT INTO `pages` (`id`, `linklabel`, `pagetitle`, `description`, `pagebody`)
-VALUES (NULL, 'Home', 'WebDeveloprz Custom CMS For PWA\'s',
-'WebDeveloprz Custom Content Management System For Websites & Progressive Web Applications',
-'This is the page body! You can put anything you want here using just text or add <strong><u>HTML</u></strong> as desired ;)')
