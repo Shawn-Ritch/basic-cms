490544f9fdbe75a4e73c0bed846b8948c0e87a28
Shawn Ritch <sr@webdeveloprz.com>
2020-08-26 03:03:51 -0700

    Update contact.php

--- inc/templates/contact.php
+++ inc/templates/contact.php
@@ -5,17 +5,15 @@ require_once('inc/header.php');
   <main>
     <section class="hero">
       <h1 class="ctxt">Contact Me</h1>
-      <div class="c50m cnt ctxt">
-        <p class="mt10">
-          <a href="mailto: contact-us@webdeveloprz.com">Send An Email</a><br>
-          Or Call<br>
-          <a href="tel: 14802833393">1(480)283&hyphen;3393</a><br>
-          <h2>Please Note&colon;</h2>
-          <p>
-            I&apos;m Very Happy To Help You With Consultations &sol; 
-            Projects&comma; But&comma; Please Keep In Mind&comma; I&apos;m Not Rich 
-            And Can&apos;t Give Away Too Much Time For Free&period;
-          </p>
+      <div class="c50m cnt mt10 ctxt">
+        <a class="mt10" href="mailto: contact-us@webdeveloprz.com">Send An Email</a><br>
+        Or Call<br>
+        <a href="tel: 14802833393">1(480)283&hyphen;3393</a><br>
+        <h2>Please Note&colon;</h2>
+        <p>
+          I&apos;m Very Happy To Help You With Consultations &sol; 
+          Projects&comma; But&comma; Please Keep In Mind&comma; I&apos;m Not Rich 
+          And Can&apos;t Give Away Too Much Time For Free&period;
         </p>
       </div>
     </section>
