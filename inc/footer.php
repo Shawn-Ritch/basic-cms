4f123b77429a4c360038536636955ba2c86c5050
Shawn Ritch <sr@webdeveloprz.com>
2020-08-26 02:35:20 -0700

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -2,8 +2,9 @@
     <div class="c50 site_map">
       <a href="<?php echo $rootUrl; ?>/sitemap.html">Sitemap</a>
     </div>
-    <div class="c50"></div>
+    <div class="c50">
       <h6>Developed By&colon; <br>
         <a target="_blank" href="https://shawn-ritch.github.io/portfolio/">Shawn Ritch</a>
       </h6>
+    </div>
   </footer>
