d052cd32abcd3a7c953dad2be02eb3d9f568b794
Shawn Ritch <sr@webdeveloprz.com>
2020-08-28 13:27:39 -0700

    Content and structure changes

--- inc/templates/about.php
+++ inc/templates/about.php
@@ -4,25 +4,37 @@ require_once('inc/header.php');
 ?>
   <main>
     <section class="hero">
-      <h1 class="ctxt">About This CMS</h1>
       <div class="c50m cnt">
+        <h1 class="ctxt">About This CMS</h1>
         <p>
-          I Developed This&comma; And Other Much More Advanced&comma; Content Management Systems 
-          For Projects And Ideas I had&comma; To Give Me An Advantage With SEO&period;
-          There Are Many Other Advantages In Creating And Using Such A Light Weight&comma; 
+          This CMS&lpar;Content Management System&rpar; Is A Fully Dynamic&comma; 
+          Database Driven&comma; One Page Runs All System&period;
+          There Are Many Other Advantages In Using Such A Lightweight&comma; 
           Highly Efficient&comma; Dynamic&comma; Fast Loading System Like This One&period;&period;&period;
+        </p>
+        <h2>Advantages</h2>
+        <p>
+          &bull; Renders Clean&comma; Search Engine Friendly URL&apos;s&period;<br>
+          &bull; Utilizes One Page To Run Every Page Of A Website Or App&period;<br>
+          &bull; A Major Time savings Over Creating Static HTML Pages&period;<br>
+          &bull; No More Isuues Linking Or Rewrighting Code Over And Over&period;<br>
+          &bull; The Ability to inject PHP Into Any Code You Write &hyphen; Anywhere&period;<br>
+          &bull; Much Easier To Navigate Through To Add Or Modify Things&period;<br>
+          &bull; AND&comma; Is Dynamic Page Template Ready&comma; For&colon;<br>
+          &#9702; eCommerce &#9702; Social Networks &#9702; Blogs 
+          &#9702; Video&comma; Photo&comma; And Many Other Types Of Portfolios Or Applications
         </p>
         <p>
         Need Help With A Project&comma; <a href="<?php echo $rootUrl; ?>/contact">Contact</a> Me&period;
         </p>
-        <h3 class="ctxt">Notice&colon;</h3>
-        <h4>Disclaimer&colon;</h4>
+        <h2>Notice&colon;</h2>
         <p>
-          This Content Management System Is Meant For Educational Purposes.
+          This Content Management System Is Meant For Educational Purposes And May Not Be Entirely Secure&period;
           If You Use This Live Online With A Hosting Provider&comma; You Do So At Your Own Risk&period;
           If You Use This System For Anything Other Than Its Intended Purpose&comma;
           &apos;I&apos; &lpar;Shawn Ritch&rpar; Or &apos;We&apos; &lpar;WebDeveloprz&rpar; Cannot Be Held Liable For Any Damages&comma;
-          Or Loss Of Any Kind&period;
+          Or Loss Of Any Kind&period; See More At 
+          <a target="_blank" href="https://github.com/Shawn-Ritch/basic-cms">https://github.com/Shawn-Ritch/basic-cms</a>
         </p>
       </div>
     </section>
